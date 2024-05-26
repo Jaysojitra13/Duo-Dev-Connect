383defdba351d8652ca278f29962a01cb9422e57
jaysojitra13 <jaysojitra13@gmail.com>
2024-05-26 12:12:03 -0400

    feat: setup next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
-import { ModeToggle } from "@/components/mode-toggle";
+import { Providers } from "./provider";
+import Home from "./page";
+import { Header } from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div>
-              <ModeToggle/>
-            </div>
-            {children}
-          </ThemeProvider>
-        </body>
+        <Providers>
+          <Header />
+          <Home />
+        </Providers>
+      </body>
     </html>
   );
 }
