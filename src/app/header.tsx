10c6f9df75a04ff794c9909461ea0ce93ba10670
jaysojitra13 <jaysojitra13@gmail.com>
2024-05-26 21:27:07 -0400

    Change header UI

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,17 +3,60 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogIn, LogOut } from "lucide-react";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+
+function AccountDropDown() {
+  const session = useSession();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ?? ""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {session?.data ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOut className="mr-2" />
+            Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogIn className="mr-2" />
+            Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export function Header() {
   const session = useSession();
   return (
-    <div>
-      {session?.data ? (
-        <Button onClick={() => signOut()}>Sign Out</Button>
-      ) : (
-        <Button onClick={() => signIn("google")}>Sign In</Button>
-      )}
-      <ModeToggle />
-    </div>
+    <header className="bg-gray-100 dark:bg-gray-900 container mx-auto py-2 ">
+      <div className="flex justify-between items-center">
+        <Link href="/" className="text-xl">
+          DuoDevConnect
+        </Link>
+        <div className="flex items-center gap-4">
+          <AccountDropDown />
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
   );
 }
